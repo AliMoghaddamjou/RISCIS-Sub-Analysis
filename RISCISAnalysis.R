50225de72a35ab65b754ac1c58f14637c14a4e79
AliMoghaddamjou <99236531+AliMoghaddamjou@users.noreply.github.com>
2022-02-24 23:59:10 -0500

    Update RISCISAnalysis.R
    Added the code to load and install packages. Added the first code block to graph data

--- RISCISAnalysis.R
+++ RISCISAnalysis.R
@@ -1,10 +1,37 @@
-#################################
-####RISCIS  ANALYSIS#############
-#######Prepared BY:##############
-###########ALI MOGHADDAMJOU######
+############################################################################
+############################################################################
+###                                                                      ###
+###                           RISCIS ANALYSIS                            ###
+###                           ALI MOGHADDAMJOU                           ###
+###                                                                      ###
+############################################################################
+############################################################################
+
+# clear console
+cat("\f")
+
+# clear environment
+# be careful because it is irreversible!
+rm(list = ls())
+
+##Function to check if required packages are installed 
+requiredPackages <- c("gplots", "jtools", "ggstance", "patchwork", "lme4", "splines", "splines", "broom.mixed", "MASS", "export_summs", "evd")
+packagesLoading <- function(pkg){
+  new.pkg <- pkg[!(pkg %in% installed.packages()[, "Package"])]
+  if (length(new.pkg))
+    install.packages(new.pkg, dependencies = TRUE)
+  sapply(pkg, require, character.only = TRUE)
+}
+
+packagesLoading(requiredPackages) ## Displaying if Packages installed
+
+
+##Loading Package. This is wrapped in invisible to not display all packages loaded by user
+invisible(lapply(requiredPackages, library, character.only = TRUE))
+
 
 ##SET WD THE FOLDER WITH THE DATAIN
-
+#setwd("C:/Users/Ali/OneDrive/Projects/RISCIS/IntermAnalysisFeb28/gitRepository/data")
 
 ##Loading the data from the CSV file
 ##Please refer to stata do file in repository for the coes to obtain the file loaded here
@@ -31,3 +58,64 @@ dataASIA <- split(data, f = data$baseASIA)
 ASIAA <- dataASIA[["A"]]
 ASIAB <-dataASIA[["B"]]
 ASIAC <-dataASIA[["C"]]
+
+
+###############CODE BLOCK 1##################
+#######Creating Graphs of Endpoints##########
+
+##Graphs #######################################
+##First Create Subset Data in long form 
+
+graphDataAll <- data[c("trtgrp", "UEMDiff6m", "LEMDiff6m", "TOTMDiff6m")] %>%
+  pivot_longer(-trtgrp, names_to = "variables", values_to = "value")  
+
+longA <- ASIAA[c("trtgrp", "UEMDiff6m", "LEMDiff6m", "TOTMDiff6m")] %>%
+  pivot_longer(-trtgrp, names_to = "variables", values_to = "value")  
+longB <- ASIAB[c("trtgrp", "UEMDiff6m", "LEMDiff6m", "TOTMDiff6m")] %>%
+  pivot_longer(-trtgrp, names_to = "variables", values_to = "value")  
+longC <- ASIAC[c("trtgrp", "UEMDiff6m", "LEMDiff6m", "TOTMDiff6m")] %>%
+  pivot_longer(-trtgrp, names_to = "variables", values_to = "value")  
+
+
+# Create the plot
+
+##All
+AllGraph <- ggboxplot(
+  graphDataAll, x = "trtgrp", y = "value",
+  fill = "trtgrp", palette = "npg", legend = "none", 
+  ggtheme = theme_pubr(border = TRUE)
+)+
+  facet_wrap(~variables) +  ggtitle("All Patients") +
+  theme(plot.title = element_text(hjust = 0.5)) + xlab("Treatment") +ylab(NULL)
+
+##A
+AGraph <- ggboxplot(
+  longA, x = "trtgrp", y = "value",
+  fill = "trtgrp", palette = "npg", legend = "none", 
+  ggtheme = theme_pubr(border = TRUE)
+)+
+  facet_wrap(~variables) +  ggtitle("ASIA A") +
+  theme(plot.title = element_text(hjust = 0.5)) + xlab("Treatment")+ylab(NULL)
+
+##B
+BGraph <- ggboxplot(
+  longB, x = "trtgrp", y = "value",
+  fill = "trtgrp", palette = "npg", legend = "none", 
+  ggtheme = theme_pubr(border = TRUE)
+)+
+  facet_wrap(~variables) +  ggtitle("ASIA B") +
+  theme(plot.title = element_text(hjust = 0.5)) + xlab("Treatment")+ylab(NULL)
+##C
+CGraph <- ggboxplot(
+  longC, x = "trtgrp", y = "value",
+  fill = "trtgrp", palette = "npg", legend = "none", 
+  ggtheme = theme_pubr(border = TRUE)
+)+
+  facet_wrap(~variables) +  ggtitle("ASIA C") +
+  theme(plot.title = element_text(hjust = 0.5)) + xlab("Treatment")+ylab(NULL)
+
+
+AllGraph +AGraph +BGraph +CGraph
+
+
+
