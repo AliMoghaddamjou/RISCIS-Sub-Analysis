8169b8c0780a3f9bab5c15bc905467bfed834171
AliMoghaddamjou <99236531+AliMoghaddamjou@users.noreply.github.com>
2022-02-24 23:45:26 -0500

    Update RISCISAnalysis.R
    defining outcomes and treatments

--- RISCISAnalysis.R
+++ RISCISAnalysis.R
@@ -13,3 +13,13 @@ baseData<-read.csv('alldata.csv')
 
 ## Replacing the race value 99 to missing 
 baseData$RACE[baseData$RACE == 99]<- NA
+
+
+##DEFINING DATA TREATMENT AND OUTCOME
+
+data<-baseData ##data
+
+Treatment <- data$trtgrp  ##unblinded treatment group 
+
+##Outcome. UEM is used in this case. This can be changed to LEMDiff6m or TOTMDiff6m for the other outcomes 
+Outcome <- data$UEMDiff6m 
