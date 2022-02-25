1a7563f3965f9657bbac16a2fbea270423226226
AliMoghaddamjou <99236531+AliMoghaddamjou@users.noreply.github.com>
2022-02-24 23:43:09 -0500

    Update RISCISAnalysis.R
    added the lines to load data

--- RISCISAnalysis.R
+++ RISCISAnalysis.R
@@ -2,3 +2,10 @@
 ####RISCIS  ANALYSIS#############
 #######Prepared BY:##############
 ###########ALI MOGHADDAMJOU######
+
+##SET WD THE FOLDER WITH THE DATAIN
+
+
+##Loading the data from the CSV file
+##Please refer to stata do file in repository for the coes to obtain the file loaded here
+baseData<-read.csv('alldata.csv')
